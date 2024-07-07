b83c88254b0b2d4f5828aa9304e73a8441f6bd27
AlessioLaTorre <ltrlss02p04g791p@studenti.unical.it>
2024-07-07 17:05:03 +0200

    commit Domanda

---
+++ src/main/java/GestioneAppello/Domanda.java
@@ -0,0 +1,41 @@
+package GestioneAppello;
+
+import java.util.ArrayList;
+
+public class Domanda {
+    private final String testo;
+    private final String rispostaCorretta;
+    private final ArrayList<String> risposteSbagliate;
+    private static final int numRisposte = 4;
+
+
+    public Domanda(String testo, String rispostaCorretta, ArrayList<String> risposteSbagliate) {
+        this.testo = testo;
+        this.rispostaCorretta = rispostaCorretta;
+        this.risposteSbagliate = risposteSbagliate;
+    }
+
+    public String getTesto() {
+        return testo;
+    }
+
+    public String getRispostaCorretta() {
+        return rispostaCorretta;
+    }
+
+    public ArrayList<String> getRisposteSbagliate() {
+        return new ArrayList<>(risposteSbagliate);
+    }
+
+    public ArrayList<String> getRisposte()
+    {
+        ArrayList<String> risposte = new ArrayList<>();
+        risposte.add(rispostaCorretta);
+        risposte.addAll(risposteSbagliate);
+        return risposte;
+    }
+
+    public int getNumRisposte() {
+        return numRisposte;
+    }
+}
